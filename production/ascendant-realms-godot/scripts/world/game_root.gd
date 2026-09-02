8990fc2cc284101f08bc65ba79cb6a058c605329
Jardas33 <moreiraquim@gmail.com>
2026-09-02 12:30:26 -0400

    Ascendant Realms Task756 R5 repair capture dispatch

--- production/ascendant-realms-godot/scripts/world/game_root.gd
+++ production/ascendant-realms-godot/scripts/world/game_root.gd
@@ -121,7 +121,9 @@ func _ready() -> void:
 		tut_layer.add_child(tutorial)
 		tutorial.setup(world, rts)
 	_m20_end(ready_stage)
-	if OS.get_environment("ASCENDANT_CURRENT_WORKER_ECONOMY_CAPTURE") == "1":
+	if OS.get_environment("ASCENDANT_CURRENT_MILITARY_PRODUCTION_CAPTURE") == "1":
+		call_deferred("_start_current_military_production_capture")
+	elif OS.get_environment("ASCENDANT_CURRENT_WORKER_ECONOMY_CAPTURE") == "1":
 		call_deferred("_start_current_worker_economy_capture")
 	elif OS.get_environment("ASCENDANT_CURRENT_SHORT_PUBLIC_CAPTURE") == "1":
 		call_deferred("_start_current_short_public_capture")
@@ -161,6 +163,11 @@ func _start_current_short_public_capture() -> void:
 
 func _start_current_worker_economy_capture() -> void:
 	var runner = get_node_or_null("/root/CurrentWorkerEconomyCapture")
+	if runner and runner.has_method("capture_gameplay"):
+		runner.capture_gameplay(self)
+
+func _start_current_military_production_capture() -> void:
+	var runner = get_node_or_null("/root/CurrentMilitaryProductionCapture")
 	if runner and runner.has_method("capture_gameplay"):
 		runner.capture_gameplay(self)
 
